73911092909f89ca778a781e0c0e301ab07c553b
Vidhi Sheth <vtsheth07@gmail.com>
2023-07-03 10:52:50 -0400

    Issue#7-Implemented insert_product function and its unit tests

--- Backend/products.py
+++ Backend/products.py
@@ -39,6 +39,35 @@ class Products:
             })
         # Return the response list
         return response
+    
+    #@contract
+    #@pre(lambda product: isinstance(product, dict), "The product must be a dictionary.")
+    #@pre(lambda product: 'name' in product and 'unit_of_measure_id' in product and 'price_per_unit' in product,
+     #    "The product dictionary must contain 'name', 'unit_of_measure_id', and 'price_per_unit' keys.")
+    #@post(lambda result: isinstance(result, int), "The return value must be an integer.")
+    def insert_new_product(self, product):
+        """
+        Inserts a new product into the database.
+
+        @param product: A dictionary containing the product information.
+        @return: The ID of the inserted product.
+        """
+
+        # Create a cursor object to execute SQL queries
+        cursor = self.connection.cursor()
+        # SQL query to insert data into the 'products' table
+        query = ("INSERT INTO products "
+                 "(name, unit_of_measure_id, price_per_unit)"
+                 "VALUES (%s, %s, %s)")
+        # Data to be inserted into the table
+        data = (product['name'], product['unit_of_measure_id'], product['price_per_unit'])
+
+        # Execute the SQL query with the provided data
+        cursor.execute(query, data)
+        # Commit the changes to the database
+        self.connection.commit()
+        # Return the last inserted row ID
+        return cursor.lastrowid
 
 def main():
     connection = SQLConnection()
@@ -46,6 +75,11 @@ def main():
 
     products = Products(connection)
     products.get_all_products()
+    products.insert_new_product({
+        'name': 'potatoes',
+        'unit_of_measure_id': '1',
+        'price_per_unit': 10
+    })
 
 if __name__ == '__main__':
     main()
