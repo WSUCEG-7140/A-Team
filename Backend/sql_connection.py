ca1ad8b8dbdc3c2f32afabfb01e80a228f0433ba
Vidhi Sheth <vtsheth07@gmail.com>
2023-07-02 23:39:57 -0400

    Issue#6-Added proudcts.py file with Product class and implemented get_all_product function

--- Backend/sql_connection.py
+++ Backend/sql_connection.py
@@ -1,15 +1,34 @@
 import mysql.connector
+#from contracts import contract, pre, post
 
 class SQLConnection:
     def __init__(self):
         self.connection = None
 
-    def connect(self, user, password, database):
+    #@contract
+    #@pre(lambda self: self.connection is None, "Connection must not be already established.")
+    #@post(lambda self, result: self.connection is not None and result is self.connection,
+        # "Connection must be established and returned.")
+    def connect(self):
+        """
+        Connects to the MySQL database.
+
+        @return The MySQL connection object.
+        """
+        user = 'root'  # Set your MySQL username here
+        password = 'root'   # Set your MySQL password here
+        database = 'grocery_store'  # Set the name of your database here
         if self.connection is None:
             self.connection = mysql.connector.connect(user=user, password=password, database=database)
         return self.connection
 
+    #@contract
+    #@pre(lambda self: self.connection is not None, "Connection must be established.")
+    #@post(lambda self: self.connection is None, "Connection must be closed.")
     def close(self):
+        """
+        Closes the MySQL connection.
+        """
         if self.connection is not None:
             self.connection.close()
             self.connection = None
