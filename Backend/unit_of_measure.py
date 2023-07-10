a4a3f84bad87989842ad1db8a374877e65e477aa
Mousami <moushmi.Khadilkar@outlook.com>
2023-07-10 15:01:06 -0400

    Implemented the code changes for Issue 71.

--- Backend/unit_of_measure.py
+++ Backend/unit_of_measure.py
@@ -10,28 +10,28 @@ class unit_of_measure:
         """
         self.connection = connection
 
-# @contract
-# @post(lambda result: isinstance(result, list), "The return value must be a list.")
-def get_unit_of_measure(connection):
-    # Create a cursor object to execute SQL queries
-    cursor = connection.cursor() 
-    # SQL query to retrieve unit of measure 
-    query = "SELECT unit_of_measure_id, unit_of_measure_name FROM unit_of_measure"  
-    # Execute the query using the cursor
-    cursor.execute(query)  
-    response = []
-    # Iterate over the query results
-    for unit_of_measure_id, unit_of_measure_name in cursor:  
-        # Create a dictionary for each unit of measure
-        response.append({  
-            # Store the uom_id
-            'unit_of_measure_id': unit_of_measure_id,  
-            # Store the uom_name
-            'unit_of_measure_name': unit_of_measure_name  
-        })
-    # Return the list of unit of measure dictionaries
-    return response  
+    # @contract
+    # @post(lambda result: isinstance(result, list), "The return value must be a list.")
+    def get_unit_of_measure(self):
+        # Create a cursor object to execute SQL queries
+        cursor = self.connection.cursor() 
+        # SQL query to retrieve unit of measure 
+        query = ("SELECT * FROM unit_of_measure") 
+        # Execute the query using the cursor
+        cursor.execute(query)  
+        response = []
+        # Iterate over the query results
+        for unit_of_measure_id, unit_of_measure_name in cursor:  
+            # Create a dictionary for each unit of measure
+            response.append({  
+                # Store the unit_of_measure_id
+                'unit_of_measure_id': unit_of_measure_id,  
+                # Store the unit_of_measure_name
+                'unit_of_measure_name': unit_of_measure_name  
+            })
+        # Return the list of unit of measure dictionaries
+        return response  
 
 #if __name__ == '__main__':
-    #from sql_connection import SQLConnection
+#       main()
 
